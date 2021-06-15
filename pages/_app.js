d6699288a70ccf4e2777770ab43b9e2230735215
gOps132 <epilred@icloud.com>
2021-06-15 15:20:44 +0800

    Added some confetti

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import Nav from "../components/nav";
+import Scbtn from "../components/scroll_to_top";
+import Footer from "../components/footer";
+
+import React, { useState, useCallback } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	// starts with light
+	const theme_map = {
+		light: 'dark',
+		solar: 'light',
+		dark: 'solar'
+	};
+
+
+	let [currentTheme, useTheme] = useState(
+		(Object.keys(theme_map)[0]));
+
+	const toggle_theme = useCallback(() => {
+		useTheme(theme_map[currentTheme]);
+	});
+
+	return (
+		<div className={currentTheme}>
+			<Nav theme={currentTheme} theme_callback={toggle_theme} theme_map={theme_map} />
+			<false_body></false_body>
+			<article>
+				<div className={`inner_root`}>
+					<Component {...pageProps} />
+				</div>
+				<Footer />
+			</article>
+			<Scbtn/>
+		</div>
+	)
 }
 
 export default MyApp
