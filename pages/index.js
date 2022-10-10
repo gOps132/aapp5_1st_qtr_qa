df028f906af57689b5cb1eb9ac7f6d6bc49cc878
Gian Cedrick G. Epilan <epilred@icloud.com>
2022-10-10 17:59:30 +0800

    progress has been made

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,19 @@ import Head from 'next/head';
 import home_styles from '../styles/Home.module.css';
 
 import React, { useState, useEffect, useRef } from "react";
+import GoogleMapReact from 'google-map-react';
 
 export default function Home(props) {
+	const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+	const defaultProps = {
+		center: {
+			lat: 10.99835602,
+			lng: 77.01502627
+		},
+			zoom: 11
+	};
+
 	return (
 		<>
 			<Head>
@@ -16,6 +27,19 @@ export default function Home(props) {
 			</Head>
 			<main className={home_styles.main_div}>
 				{/* map google map to the place */}
+				<div style={{ height: '100vh', width: '100%' }}>
+					<GoogleMapReact
+						bootstrapURLKeys={{ key: "" }}
+						defaultCenter={defaultProps.center}
+						defaultZoom={defaultProps.zoom}
+					>
+						<AnyReactComponent
+							lat={59.955413}
+							lng={30.337844}
+							text="My Marker"
+						/>
+					</GoogleMapReact>
+				</div>
 			</main>
 		</>
 	)
