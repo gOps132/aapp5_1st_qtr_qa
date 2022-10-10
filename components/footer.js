87b82fb84121a3b9636e01b6bce3b8b32b4cd030
Gian Cedrick G. Epilan <epilred@icloud.com>
2022-10-10 17:00:39 +0800

    added appropriate data

--- components/footer.js
+++ components/footer.js
@@ -4,19 +4,23 @@ const Footer = () => {
 	return (
 		<div className={footer_style.m_footer}>
 			<div className={footer_style.m_footer_elm}>
-				<h2>Contacts</h2>
-				<h3>School Email</h3>
-				<p>giancedrick.epilan@dbtc-cebu.edu.ph</p>
+				<h2>Members</h2>
+				<h3>GROUP NULL</h3>
+				{/* <br></br> */}
+				<p>Epilan Gian Cedrick G.</p>
+				<p>Condor Chino Miguel</p>
+				<p>Johann Terence Jumao-as</p>
 			</div>
 			<div className={footer_style.m_footer_elm}>
 				<h2>INFO</h2>
-				<p>
-					made by Gian Cedrick
-				</p>
+				<h3>11-VANDOR</h3>
+				{/* <br></br> */}
+				<p>1st Qtr AAPP QA</p>
 			</div>
 			<div className={footer_style.m_footer_elm}>
 				<h2>Made with</h2>
 				<h3>Nextjs Framework</h3>
+				{/* <br></br> */}
 				<p>
 					<a href="https://nextjs.org/"><u>here</u></a>
 				</p>
